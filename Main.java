31c94f6949cf6721a761e88744f3926d0cfea9c1
adamaxy <138890873+adamaxy@users.noreply.github.com>
2023-08-01 00:06:33 +0200

    Update Main.java
    correzione

--- Main.java
+++ Main.java
@@ -1,41 +1,11 @@
-import java.util.Arrays;
-
-public class House {
-    private int floorsNumber;
-
-    private String address;
-
-    private String[] residentsNames;
-
-
-    public int getFloorsNumber() {
-        return floorsNumber;
-    }
-
-    public void setFloorsNumber(int floorsNumber) {
-        this.floorsNumber = floorsNumber;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String[] getResidentsNames() {
-        return residentsNames;
-    }
-
-    public void setResidentsNames(String[] residentsNames) {
-        this.residentsNames = residentsNames;
-    }
-
-
-    public void houseDetails(){
-        System.out.println(String.format("The number of the floor %d", getFloorsNumber()));
-        System.out.println(String.format("the address is %s", getAddress()));
-        System.out.println(Arrays.toString(getResidentsNames()));
+public class Main {
+    public static void main(String[] args) {
+        String[] residentes = {"Devy", "Salvo", "Peppe"};
+        House casa = new House();
+        casa.setFloorsNumber(9);
+        casa.setAddress("Via Uruguay 30/2");
+        
+        casa.setResidentsNames(residentes);
+        casa.houseDetails();
     }
 }
