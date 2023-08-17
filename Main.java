fa74364ad45dccf841f2cb2bd51296a8d21853ac
adamaxy <138890873+adamaxy@users.noreply.github.com>
2023-08-17 19:12:02 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,25 +1,13 @@
-import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args) {
-        House house1 = new House("","","") ;
+public class House {
+    String floorsNumber;
+    String address;
+    String residentsNames;
 
-
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("chi abita in questa casa?");
-        String residenti = scanner.nextLine();
-        System.out.println("quale è l'indirizzo?");
-        String indirizzo = scanner.nextLine();
-        System.out.println("che piano è?");
-        String piano = scanner.nextLine();
-
-      //indirizzo = house1.address;
-      //residenti =  house1.residentsNames;
-      //piano = house1.floorsNumber;
-
-      System.out.println("indirizzo: " + indirizzo + "\n" + "nome residenti " + residenti + "\n" + "piano " + piano);
-
-
+    House(String floorsNumber, String address, String residentsNames){
+        this.address= address;
+        this.floorsNumber = floorsNumber;
+        this.residentsNames = residentsNames;
     }
 
     }
