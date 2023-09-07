b6be92f77764ef02181f2285d80f9ca1d35d5c57
adamaxy <138890873+adamaxy@users.noreply.github.com>
2023-09-07 19:06:28 +0200

    Update House.java

--- House.java
+++ House.java
@@ -1,41 +1,10 @@
-import java.util.Arrays;
-
-public class House {
-private int floorsNumber;
-private String address;
-private String[] residentsNames;
-  
-  House(String floorsNumber, String address, String residentsNames){
-        this.address= address;
-        this.floorsNumber = floorsNumber;
-        this.residentsNames = residentsNames;
-
-
-    public int getFloorsNumber() {
-        return floorsNumber;
+public class House{
+    private String address;
+    private int numberOfFloors;
+    public House(String address,int numberOfFloors){
+        this.address=address;
+        this.numberOfFloors=numberOfFloors;
+        System.out.println("a new House object was created with the following data: \n"+"Address: "+address+"\nNumber of floors: "+numberOfFloors);
     }
 
-    public void setFloorsNumber(int floorsNumber) {
-        this.floorsNumber = floorsNumber;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String[] getResidentsNames() {
-        return residentsNames;
-    }
- public void setResidentsNames(String[] residentsNames) {
-        this.residentsNames = residentsNames;
-    }
-    public void houseDetails(){
-        System.out.println(String.format("The number of the floor %d", getFloorsNumber()));
-        System.out.println(String.format("the address is %s", getAddress()));
-        System.out.println(Arrays.toString(getResidentsNames()));
-    }
-}
+        }
