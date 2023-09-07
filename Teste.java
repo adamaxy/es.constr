621071042c45085dd5fe17786ea129ca9ae9d890
adamaxy <138890873+adamaxy@users.noreply.github.com>
2023-09-07 19:07:59 +0200

    Create Teste.java

---
+++ Teste.java
@@ -0,0 +1,7 @@
+public class Tester {
+    public static void main(String[] args){
+        House house1,house2;
+        house1=new House("Via crispi 1",2);
+        house2=new House("Viale italia 2",2);
+    }
+}
