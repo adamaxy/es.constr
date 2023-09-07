ba413f477104b0dc561b3eaa29c5982090a5b54b
adamaxy <138890873+adamaxy@users.noreply.github.com>
2023-09-07 19:05:12 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,11 +5,7 @@ public class Main {
     String address;
     String residentsNames;
 
-    House(String floorsNumber, String address, String residentsNames){
-        this.address= address;
-        this.floorsNumber = floorsNumber;
-        this.residentsNames = residentsNames;
+    
     }
 
-    }
 }
